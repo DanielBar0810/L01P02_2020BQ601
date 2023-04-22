02cafbe171c47563ff4dbe5e0c290c746db8fc5d
Daniel <daniel.barrera@catolica.edu.sv>
2023-04-22 10:56:22 -0600

    Add project files.

---
+++ L01P02_2020BQ601/Models/restauranteDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using L01P02_2020BQ601.Models;
+namespace L01P02_2020BQ601.Models
+
+{
+    public class restauranteDbContext : DbContext
+    {
+
+        public restauranteDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<pedidos> pedidos { get; set; }
+
+        public DbSet<platos> platos { get; set; }
+
+        public DbSet<motoristas> motoristas { get; set; }
+
+        public DbSet<clientes> clientes { get; set; }
+
+
+
+    }
+}
+
